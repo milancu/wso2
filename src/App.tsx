66aeadc0173c1e4b87cc149ac98cb03bc0a69a60
Milan Cu (Phuong Dong) <cuphuon3@fel.cvut.cz>
2022-08-18 16:05:27 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import {useAuthContext} from "@asgardeo/auth-react";
 import {ReactElement} from "react";
 
-// Encapsulate your components with the `AuthProvider`.
 export const App = (): ReactElement => {
 
 	const {state, signIn, signOut} = useAuthContext();
 
 	return (
 		<div className="App">
-			{
-				state.isAuthenticated
-					? (
-						<div>
-							<ul>
-								<li>{state.username}</li>
-							</ul>
-
-							<button onClick={() => signOut()}>Logout</button>
-						</div>
-					)
-					: <button onClick={() => signIn()}>Login</button>
-			}
+			{state.isAuthenticated
+				?
+				(
+					<div>
+						<div>{state.username}</div>
+						<button onClick={() => signOut()}>Logout</button>
+					</div>
+				)
+				:
+				<button onClick={() => signIn()}>Login</button>}
 		</div>
 	)
 }
